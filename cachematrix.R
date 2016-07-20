88267aabe5e7614c6b376fa7b740af2324c8f5d3
Brijraj singh <brijrajsingh@gmail.com>
2016-07-20 15:55:23 +0530

    Updated with the function definitions along with test cases

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## takes an invertible matrix and returns an object which has functions
+## for storing its value and inverse in order set,get,setinverse,getinverse
 
-## Write a short comment describing this function
+## create an invertible matrix (invertible matrix is a square matrix)
+## a <- matrix(1:4,2,2)
+## test cacheMtrxVar <- makeCacheMatrix(a)
+## test cacheSolveVar <- cacheSolve(makeCacheMatrix(a))
+## output cacheSolveVar
+        ##      [,1] [,2]
+        ## [1,]   -2  1.5
+        ## [2,]    1 -0.5
 
+## reverse it back to test 
+## c <- cacheSolve(makeCacheMatrix(cacheSolveVar))
 makeCacheMatrix <- function(x = matrix()) {
+  # Set inverse of X to NULL
+  xInverseVar <- NULL
 
+  # function to set the value of x 
+  set <- function(y) {
+    x <<- y
+    xInverseVar <<- NULL
+  }
+
+  #function to return the value as it is of x
+  get <- function() x
+
+  #function to set the value of inversevariable
+  setInverse <- function(inverse) xInverseVar <<- inverse
+
+  #function to get the inverseVar
+  getInverse <- function() xInverseVar
+
+  #List is returned with set, set, setInverse,getInverse functions
+  list(set = set, get = get,setInverse = setInverse,getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Takes input of makeCacheMatrix object, 
+## if the inverse doesn't exist check by getInverse function, then set it 
+## using the setInverse function, calculate the inverse using solve(matrixObject) function 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        xInverse <- x$getInverse()
+
+        #Check if the given inverse is not null, if not null then return it
+        if(!is.null(xInverse)) {
+        print("Cached Value of Invertible matrix...")
+        return(xInverse)
+        }
+
+        #comes here only if xInverse was found null, get matrix, use solve function, 
+        #set to using setInverse function and return the xInverse object  
+        matrixObj <- x$get()
+        xInverse <- solve(matrixObj, ...)
+        x$setInverse(xInverse)
+        xInverse
 }
